3ea8d446ec29b9b45fdb9bf14eca02ee60c93f8b
Ian-Almeidaa <164452386+Ian-Almeidaa@users.noreply.github.com>
2024-10-15 15:04:50 +0000

    desenvolvimento até a questão 18

--- lista2.py
+++ lista2.py
@@ -96,4 +96,92 @@ def q12():
         print('você é idoso')
 
 def q13():
+    name = input('Digite o seu nome: ')
+    nota1 = int(input('Digite a primeira nota: '))
+    nota2 = int(input('Digite a segunda nota: '))
+    if((nota1 + nota2)/2 >= 7):
+        print(f'Muito bem {name} você foi aprovado com a média de {(nota1 + nota2)/2}')
+    elif ((nota1 + nota2)/2 >= 4):
+        print(f'Tome cuidado {name} você está de prova final com a média de {(nota1 + nota2)/2}')
+    else:
+        print(f'{name}, infelizmente você está reprovado com a média de {(nota1 + nota2)/2}')
+
+def q14():
+    salary = round(float(input('Digite o seu salário: ')),2)
+    if salary > 2000.0:
+        print(f'O desconto do seu salário é de 30% {salary - (salary/10)*7}, o valor final dele é de {salary - (salary/10)*3}')
+    elif salary > 1200.0:
+        print(f'O desconto do seu salário é de 25% {salary - (salary/10)*7.5}, o valor final dele é de {salary - (salary/10)*2.5}')
+    elif salary > 600.0:
+        print(f'O desconto do seu salário é de 20% {salary - (salary/10)*8}, o valor final dele é de {salary - (salary/10)*2}')
+    else:
+        print(f'seu salário está isento de INSS, se mantendo em {salary}')
+
+def q15():
+    product = round(float(input('Digite o valor do produto: ')),2)
+    if product >= 20.0:
+        print(f'Para atingir 45% da margem de lucro, o produto será vendido no valor de {product + (product/10)*4.5}')
+    else:
+        print(f'Para atingir 30% da margem de lucro, o produto será vendido no valor de {product + (product/10)*3}')
+
+def q16():
+    age = int(input('Digite a sua idade: '))
+    if age >= 18:
+        print('Você competirá na categoria Sênior')
+    elif age >= 14:
+        print('Você competirá na categoria Juvenil B')
+    elif age >= 11:
+        print('Você competirá na categoria Juvenil A')
+    elif age >= 8:
+        print('Você competirá na categoria Infantil B')
+    else:
+        print('Você competirá na categoria Infantil A')
+
+def q17():
+    name = input('Digite o seu nome: ')
+    age = int(input('Digite a sua idade: '))
+    if age > 65:
+        print(f'Olá {name}, por conta da sua idade ({age}) o seu plano seria R$400,00')
+    elif age > 59:
+        print(f'Olá {name}, por conta da sua idade ({age}) o seu plano seria R$250,00')
+    elif age > 45:
+        print(f'Olá {name}, por conta da sua idade ({age}) o seu plano seria R$150,00')
+    elif age > 29:
+        print(f'Olá {name}, por conta da sua idade ({age}) o seu plano seria R$120,00')
+    elif age > 10:
+        print(f'Olá {name}, por conta da sua idade ({age}) o seu plano seria R$60,00')
+    else:
+        print(f'Olá {name}, por conta da sua idade ({age}) o seu plano seria R$30,00')
+
+def q18():
+    year = int(input('Digite o número do mês desejado: '))
+    match year:
+        case 1:
+            print(f'O mês {year} representa Janeiro')
+        case 2:
+            print(f'O mês {year} representa Fevereiro')
+        case 3:
+            print(f'O mês {year} representa Março')
+        case 4:
+            print(f'O mês {year} representa Abril')
+        case 5:
+            print(f'O mês {year} representa Maio')
+        case 6:
+            print(f'O mês {year} representa Junho')
+        case 7:
+            print(f'O mês {year} representa Julho')
+        case 8:
+            print(f'O mês {year} representa Agosto')
+        case 9:
+            print(f'O mês {year} representa Setembro')
+        case 10:
+            print(f'O mês {year} representa Outubro')
+        case 11:
+            print(f'O mês {year} representa Novembro')
+        case 12:
+            print(f'O mês {year} representa Dezembro')
+        case _:
+            print(f'O mês {year} não é válido')
+
+def q19():
     
