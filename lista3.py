2b2485149822de415fa1042bd35890dd7f713d73
Ian-Almeidaa <164452386+Ian-Almeidaa@users.noreply.github.com>
2024-10-25 13:09:58 +0000

    ex 9

--- lista3.py
+++ lista3.py
@@ -41,13 +41,39 @@ def q6():
         i+=1
 
 def q7():
-    for _ in range (3):
+    for _ in range (15):
         aluno = input('Digite o nome do aluno: ').strip()
         nota1 = round(float(input('Digite a sua 1° nota: ')),2)
         nota2 = round(float(input('Digite a sua 2° nota: ')),2)
         media = (nota1 + nota2) /2
         mediAll += media
         
+def q8():
+    for _ in range (5):
+        nome = input('Digite o seu nome: ')
+        erro = True
+        while erro == True:
+            salario = 0.0
+            try:
+                salario = round(float(input('Número 1: ')),2)
+            except ValueError: # só é executado para o tipo de erro ValueError
+                print('O valor informado não é um número inteiro!')
+                erro = True
+            except: # captura qualquer erro
+                print('Ocorreu um erro desconhecido! Tente novamente!')
+                erro = True
+            else:
+                erro = False
+
+        if salario < 1300.00:
+            print (f'Olá {nome}, você está isento de IRRF')
+        elif salario < 2300.00:
+            print(f'Olá {nome}, você terá um desconto de 10% de desconto do IRRF, seu salário ficará {salario - (salario/10)}')
+        else:
+            print(f'Olá {nome}, você terá um desconto de 15% de desconto do IRRF, seu salário ficará {salario - (salario/10)*1.5}')
+
+def q9():
+    
 
 questao = int(input('Questão a ser executada: '))
 eval(f'q{questao}()')
