17affa79d0f9f0b5f0c02beb4733cf0b820afda9
Ian-Almeidaa <164452386+Ian-Almeidaa@users.noreply.github.com>
2024-10-30 00:10:05 +0000

    ex 14

--- lista3.py
+++ lista3.py
@@ -43,10 +43,34 @@ def q6():
 def q7():
     for _ in range (15):
         aluno = input('Digite o nome do aluno: ').strip()
-        nota1 = round(float(input('Digite a sua 1° nota: ')),2)
-        nota2 = round(float(input('Digite a sua 2° nota: ')),2)
+        erro = True
+        while erro == True:
+            try:
+                nota1 = round(float(input('Digite a sua 1° nota: ')),2)
+            except ValueError:
+                print('O valor informado não é um número inteiro!')
+                erro = True
+            except: # captura qualquer erro
+                print('Ocorreu um erro desconhecido! Tente novamente!')
+                erro = True
+            else:
+                erro = False
+        erro = True
+        while erro == True:
+            try:
+                nota2 = round(float(input('Digite a sua 2° nota: ')),2)
+            except ValueError:
+                print('O valor informado não é um número inteiro!')
+                erro = True
+            except: # captura qualquer erro
+                print('Ocorreu um erro desconhecido! Tente novamente!')
+                erro = True
+            else:
+                erro = False 
         media = (nota1 + nota2) /2
         mediAll += media
+
+    print(f'A média final da turma é de {mediAll/15}')
         
 def q8():
     for _ in range (5):
@@ -73,7 +97,67 @@ def q8():
             print(f'Olá {nome}, você terá um desconto de 15% de desconto do IRRF, seu salário ficará {salario - (salario/10)*1.5}')
 
 def q9():
-    
+    print()
 
+def q10():
+    print()
+
+def q11():
+    num = 1
+    cont = 0
+    while num != 0:
+        erro = True
+        while erro == True:
+            num = 1.0
+            try:
+                num = round(float(input('Número: ')),2)
+            except ValueError: # só é executado para o tipo de erro ValueError
+                print('O valor informado não é um número inteiro!')
+                erro = True
+            except: # captura qualquer erro
+                print('Ocorreu um erro desconhecido! Tente novamente!')
+                erro = True
+            else:
+                erro = False
+        if num >= 100.0 and num < 200.1:
+            cont += 1
+    print(f'O total de números entre 100 e 200 foi de {cont}')
+
+def q12():
+    ctA = 5000000
+    ctB = 7000000
+    cont = 0
+    while ctA <= ctB:
+        ctA+=(5000000/100)*3
+        ctB+=(7000000/100)*2
+        cont+=1
+    print(f'O tempo necessário foi de {cont} anos')
+    
+def q13():
+    print()
+
+def q14():
+    num = 0
+    while num != 1:
+        factor = 1
+        erro = True
+        while erro == True:
+            num = 1.0
+            try:
+                num = round(int(input('Número: ')),2)
+            except ValueError: # só é executado para o tipo de erro ValueError
+                print('O valor informado não é um número inteiro!')
+                erro = True
+            except: # captura qualquer erro
+                print('Ocorreu um erro desconhecido! Tente novamente!')
+                erro = True
+            else:
+                erro = False
+        for x in range (num, 1, -1):
+            factor *= x
+        print(factor)
+
+def q15():
+    
 questao = int(input('Questão a ser executada: '))
 eval(f'q{questao}()')
